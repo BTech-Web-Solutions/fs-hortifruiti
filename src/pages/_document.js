8a78fbd4d95d35e613ff127e949f122733b71a30
brodrigues0ll <bernardorg99@hotmail.com>
2024-03-16 22:40:35 -0300

    Meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,9 +11,16 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
           rel="stylesheet"
         />
-        <meta name="description" content="FS Hortifruti" />
-        <meta name="author" content="FS Hortifruti" />
-        <meta name="keywords" content="FS Hortifruti" />
+        <meta
+          name="keywords"
+          content="hortifruti, delivery, frutas, verduras, legumes, orgânicos, saudável, fresco, entrega em casa, cesta de frutas, alimentação saudável, produtos naturais, compra de frutas online, compra de verduras online, mercado orgânico, legumes frescos, mercado saudável, delivery de alimentos, entrega de hortifruti, comprar frutas, comprar verduras"
+        />
+
+        <meta name="author" content="BTech - Web Solutions" />
+        <meta
+          name="keywords"
+          content="hortifruti, delivery, frutas, verduras, legumes, orgânicos, saudável, fresco, entrega em casa"
+        />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
