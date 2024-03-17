ba06e7ae3ce5bccdd2767fc9ccdcd39359e629f9
brodrigues0ll <bernardorg99@hotmail.com>
2024-03-16 22:37:14 -0300

    Meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,18 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+        <title>FS Hortifruti</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
           rel="stylesheet"
         />
+        <meta name="description" content="FS Hortifruti" />
+        <meta name="author" content="FS Hortifruti" />
+        <meta name="keywords" content="FS Hortifruti" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body className="font-[DM Sans]">
         <Main />
